4fb32fd2ab8aa478048d9f4c7e33d832796f07f4
Samuel <samuelldmj5@gmail.com>
2024-09-20 19:01:51 +0100

    day 13

---
+++ 30daysJavascript/day13.js
@@ -0,0 +1,52 @@
+/*
+
+Given a positive integer millis, write an asynchronous function that sleeps for millis milliseconds. It can resolve any value.
+
+ 
+
+Example 1:
+
+Input: millis = 100
+Output: 100
+Explanation: It should return a promise that resolves after 100ms.
+let t = Date.now();
+sleep(100).then(() => {
+  console.log(Date.now() - t); // 100
+});
+Example 2:
+
+Input: millis = 200
+Output: 200
+Explanation: It should return a promise that resolves after 200ms.
+ 
+
+Constraints:
+1 <= millis <= 1000
+*/
+
+
+/**
+ * @param {number} millis
+ * @return {Promise}
+ */
+
+async function sleep(millis) {
+    return new Promise(resolve => setTimeout(resolve, millis));
+}
+
+// Example 1
+let t1 = Date.now();
+sleep(100).then(() => {
+    console.log(Date.now() - t1); // Output: approximately 100
+});
+
+// Example 2
+let t2 = Date.now();
+sleep(200).then(() => {
+    console.log(Date.now() - t2); // Output: approximately 200
+});
+
+/** 
+ * let t = Date.now()
+ * sleep(100).then(() => console.log(Date.now() - t)) // 100
+ */
